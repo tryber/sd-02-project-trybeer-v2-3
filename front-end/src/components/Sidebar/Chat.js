5b7744c9899a9ad3b084ce0f5c0c9e9848e7a45d
guicgs <etohlif@protonmail.com>
2020-09-01 20:40:16 -0300

    CC

--- front-end/src/components/Sidebar/Chat.js
+++ front-end/src/components/Sidebar/Chat.js
@@ -1,20 +1,16 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { useHistory } from 'react-router-dom';
 import * as ls from '../Utils/localStorage';
-import { changePage } from '../Utils/cart';
-import Trybeer from '../../context';
 
 const ButtonChat = () => {
-  const { setOpen } = useContext(Trybeer);
   const history = useHistory();
   const { role } = ls.getItem('user') || {};
-  const route = role === 'admin' ? '/admin/messages' : '/messages';
 
   return (
     <button
       type="button"
       className="Sidebar_Button"
-      onClick={() => changePage(setOpen, history, route)}
+      onClick={() => history.push(role === 'admin' ? '/admin/messages' : '/messages')}
     >
       Messages
     </button>
